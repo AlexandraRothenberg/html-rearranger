b8c2c04f9d3ffe0ff65cf05320f989517f3d1e0f
AlexandraRothenberg <alexandra.rothenberg12@gmail.com>
2016-03-14 17:05:14 -0400

    Start of javascript

---
+++ js/main.js
@@ -0,0 +1,14 @@
+var $h1 = $('h1');
+var $main = $('main');
+var $ul = $('ul');
+var $li = $('li');
+
+$ul.remove();
+
+$h1.html(document.write('<h1>Below ground veggies</h1>'));
+
+$ul.addClass('below');
+
+$h1.html(document.write('<h1>Above ground veggies</h1>'));
+
+$ul.addClass('above');
